b7c83d5e07b73050e67b79edc45dfcc984d7d39a
Viktor Mishyn <61760650+viktormishyn@users.noreply.github.com>
2021-04-19 20:29:35 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,8 +6,8 @@ source env/bin/activate
 if [ ! -f "requirements.txt" ]; then
   echo "Installing requirements and creating requirements.txt"
   echo
-  pip install flask
-  pip install python-dotenv
+  pip install wheel
+  pip install django
   pip freeze > requirements.txt
 else
   pip install -r requirements.txt
