0f70188619d816761ab7773cba3236f51118cce6
Viktor Mishyn <viktormishyn@gmail.com>
2021-04-22 20:55:29 +0300

    Create hashing.views.view function, add redirect to home func; Add hash.html template

--- hashthat-project/hashing/views.py
+++ hashthat-project/hashing/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import HashForm
 from .models import Hash
 import hashlib
@@ -16,6 +16,12 @@ def home(request):
                 hash.text = text
                 hash.hash = text_hash
                 hash.save()
+            return redirect('hash', hash=text_hash)
 
     form = HashForm()
     return render(request, 'hashing/home.html', {'form': form})
+
+def hash(request, hash):
+    hash = Hash.objects.get(hash=hash)
+    return render(request, 'hashing/hash.html', {'hash': hash})
+
