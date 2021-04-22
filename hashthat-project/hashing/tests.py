7f0f805411710934e4fb35e3adff5f7e6723685f
Viktor Mishyn <viktormishyn@gmail.com>
2021-04-22 21:24:15 +0300

    Add hashing.tests.UnitTestCase.test_bad_data (Hash object's self.hash field validation)

--- hashthat-project/hashing/tests.py
+++ hashthat-project/hashing/tests.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from .forms import HashForm
 import hashlib
 from .models import Hash
+from django.core.exceptions import ValidationError
 
 
 class FunctionalTestCase(TestCase):
@@ -64,3 +65,12 @@ class UnitTestCase(TestCase):
         response = self.client.get(
             '/hash/2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824')
         self.assertContains(response, 'hello')
+
+    def test_bad_data(self):
+        # testing Hash object's hash field validation
+        def badHash():
+            hash = Hash()
+            hash.hash = '2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824kkkkkkkkkkkkkkkkk'
+            # hash too long
+            hash.full_clean()
+        self.assertRaises(ValidationError, badHash)
