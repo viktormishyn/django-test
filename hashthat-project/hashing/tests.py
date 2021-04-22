4849a5ed9d03eff1d9595e15244a085885180431
Viktor Mishyn <viktormishyn@gmail.com>
2021-04-22 21:58:04 +0300

    Create test_hash_ajax in hashing.tests.FunctionalTestCase

--- hashthat-project/hashing/tests.py
+++ hashthat-project/hashing/tests.py
@@ -23,6 +23,12 @@ class FunctionalTestCase(TestCase):
         self.assertIn(
             '2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824', self.browser.page_source)
 
+    def test_hash_ajax(self):
+        self.browser.get('http://localhost:8000')
+        text = self.browser.find_element_by_id('id_text')
+        text.send_keys('hello')
+        self.assertIn('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824', self.browser.page_source)
+
     def tearDown(self):
         self.browser.quit()
 
