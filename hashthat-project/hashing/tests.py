e8d966b5bb45beed993afb56ebb422ea1d119c1b
Viktor Mishyn <viktormishyn@gmail.com>
2021-04-22 15:02:16 +0300

    Create FunctionalTestCase class in hashing/tests.py

--- hashthat-project/hashing/tests.py
+++ hashthat-project/hashing/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from selenium import webdriver
 
-# Create your tests here.
+class FunctionalTestCase(TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+
+    def test_there_is_homepage(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('install', self.browser.page_source)
+
+    def tearDown(self):
+        self.browser.quit()
+
+
+
